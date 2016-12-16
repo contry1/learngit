118411be6276ec7caaa30c20b5467cbc986bb1f9
hh <1234@qq.com>
2016-12-16 15:17:16 +0800

    the fisrt many pthread demp

---
+++ pthread/pthread_many2.c
@@ -0,0 +1,59 @@
+ #include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+int quit;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t waitloc =PTHREAD_COND_INITIALIZER;
+
+void *thr_fn(void *arg)
+{
+   int i = *(int *)arg;
+   printf("thr_fn\n");
+   for (; i >0; --i)
+   {
+       /* code */
+        printf("wait i %d ms\n", i);
+   }
+   pthread_exit((void *)0);
+    
+}
+int main(int argc, char **argv) {
+    int i,err,num =10; 
+    pthread_t tid[i];
+    pthread_attr_t attr;
+    err = pthread_attr_init(&attr);
+    if (err)
+        return(err);
+    err = pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+    if (err==0){
+        for (int i = 0; i < 5; ++i)
+        {
+           err = pthread_create(&tid[i],&attr,thr_fn,(void *)&num);
+            if (err) {
+              printf("craete err\n");
+
+              }
+        }
+    }else
+        printf("set pthread detach failed\n");
+    
+    pthread_attr_destroy(&attr);
+    // printf("wait new pthread...\n");
+    sleep(5);
+
+    for (int i = 5; i >0; --i)
+    {
+      /* code */
+        printf("wait the thread:%d\n", i);
+        err = pthread_join(tid[i],(void *)&err);
+        if (!err)
+        {
+          /* code */
+            printf("wait success thread :%d\n",i);
+        }
+    }
+   
+    return 0;
+
+}
