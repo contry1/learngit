bf104e1ce581f721ca80d5136155715e30f353a8
hh <1234@qq.com>
2016-12-20 09:40:54 +0800

    the first error of extern usage

---
+++ myc/extern/extern_h.c
@@ -0,0 +1,12 @@
+#include "extern.h"
+extern int i = 1;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    for (i = 1; i < 20; ++i)
+    {
+        /* code */
+        printf("%d\n", i);
+    }
+    return 0;
+}
