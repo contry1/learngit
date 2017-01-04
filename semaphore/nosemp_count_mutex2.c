98cd04634b7ae50954261bbeb92ac12d3a43a3eb
hh <1234@qq.com>
2017-01-04 10:37:47 +0800

    the no semaphore and we use mutex example

---
+++ semaphore/nosemp_count_mutex2.c
@@ -0,0 +1,106 @@
+// semp.c
+#include <stdio.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static void thread_create(pthread_t *tid,void *(*fun)(void *));
+static void thread_wait(pthread_t tid);
+
+
+void * fun1(void *arg);
+void * fun2(void *arg);
+
+pthread_mutex_t lock;
+int sum = 0;
+int main(int argc, char const *argv[])
+{
+        pthread_mutex_init(&lock,NULL);
+        pthread_t tid1;
+        pthread_t tid2;
+       
+
+      
+        thread_create(&tid1,fun1);
+        thread_create(&tid2,fun2);
+
+       
+      
+
+        thread_wait(tid1);
+        thread_wait(tid2);
+
+
+    return 0;
+}
+
+static void thread_create(pthread_t *tid,void *(*fun)(void *))
+{
+       int res = 0;
+       res = pthread_create(tid,NULL,fun,NULL);
+       if (res == 0)
+       printf("successfully create");
+       // there sleep is must exist,or not the second thread will be  not running
+       sleep(4);
+
+}
+
+
+static void thread_wait(pthread_t tid)
+{
+    int res = 0;
+    int status = 0;
+    res = pthread_join(tid,(void *)&status);
+    if (res == 0){
+        printf("wait thread %lu successfully\n",tid);
+        printf("the return val:%d\n",status );
+        
+    }
+    else
+        perror("join thread");
+}
+
+void * fun1(void *arg)
+{
+    printf(" thread 1\n");
+    int i = 0;
+    for (; ; ) {
+        pthread_mutex_lock(&lock);
+        sum +=i;
+        // pthread_mutex_unlock(&lock);
+        i++;
+
+        // pthread_mutex_lock(&lock);
+        printf("thread 1 sum:%d\n",sum );
+        if (sum >100) {
+            
+            // break;
+        
+        }
+        pthread_mutex_unlock(&lock);
+        // there sleep time also will  interact the thread 2 output
+        sleep(2);
+
+        /* code */
+    }
+    pthread_exit((void *)0);
+}
+
+void * fun2(void *arg)
+{
+    int   i  =0;
+    printf("  thread 2\n");
+   pthread_mutex_lock(&lock);
+
+    while(sum >0)
+    {
+        sum -=i;
+        i++;
+        printf("thread 2 sum:%d\n", sum);
+    }
+    pthread_mutex_unlock(&lock);
+    sleep(1);
+
+    pthread_exit((void *)0);
+}
