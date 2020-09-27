b7ff23a6c10c25d3a43cef3d6f2175707edcc44d
contry1 <3178488406@qq.com>
2020-09-27 08:35:09 +0800

    Update vector.cpp
    编译出错

--- C_PLUSPLUS/vector.cpp
+++ C_PLUSPLUS/vector.cpp
@@ -1,1 +1,14 @@
 
+"cout": 未声明的标识符(why 'cout' is not a member of 'std')
+编译出现"cout": 未声明的标识符的错误
+
+#include<iostream>
+#include "stdafx.h"
+#include<stdlib.h>
+
+应把#include "stdafx.h"写在#include<iostream>的前面：
+
+#include "stdafx.h"
+#include<iostream>
+#include<stdlib.h>
+
