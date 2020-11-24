6807d972bad0fbbe6613518f0a3255d272251407
contry1 <3178488406@qq.com>
2020-11-24 19:42:55 +0800

    Update test.c
    new  ediotror

--- letme/test.c
+++ letme/test.c
@@ -111,3 +111,50 @@ public:
 	}
 };
 
+
+class solution3 {
+	void backtrack(vector<string> &ans, string &cur, int left, int right, int depth) {
+		if (left == 0 && right == 0)
+		{
+			ans.push_back(cur);
+			return;
+		}
+
+		if (left >right)
+		{
+			return;
+		}
+		//if (right ==0)
+		//{
+		//	cout << "hello %d:   %d:" << left << right << cur << endl;
+		//	/*if(!cur.empty())
+		//		cur.pop_back();*/
+		//	return;
+		//}
+
+	
+		if (left )
+		{
+			/*cur.push_back('(');*/
+			backtrack(ans, cur+'(', left - 1, right, depth);
+		/*	cur.pop_back();*/
+
+		}
+		if (right )
+		{
+
+			/*cur.push_back(')');*/
+			backtrack(ans, cur+')', left, right - 1, depth);
+			/*cur.pop_back();*/
+		}
+
+	}
+public:
+	vector<string>ge(int n)
+	{
+		vector<string> ans;
+		string cur;
+		backtrack(ans, cur, n, n, n);
+		return ans;
+	}
+};
