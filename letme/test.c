294a194761703a8c9ae6cf48d405431139b490e3
contry1 <3178488406@qq.com>
2020-11-24 19:16:13 +0800

    Update test.c
    new

--- letme/test.c
+++ letme/test.c
@@ -61,3 +61,53 @@ hello %d:5
 E:\vs2015_1\ltr1\Debug\ltr1.exe (进程 16652)已退出，返回代码为: 0。
 按任意键关闭此窗口...
 
+
+
+
+class solution2 {
+	void backtrack(vector<string> &ans, string &cur, int left, int right,int depth) {
+		if (left == depth && right ==depth)
+		{
+			ans.push_back(cur);
+			return;
+		}
+		
+		if (left > depth)
+		{
+			return;
+		}
+		if (right > left)
+		{
+			cout << "hello %d:   %d:" << left << right << cur << endl;
+			/*if(!cur.empty())
+				cur.pop_back();*/
+			return;
+		}
+
+		if (right < depth)
+		{
+
+			cur.push_back(')');
+			backtrack(ans, cur, left, right + 1, depth);
+			cur.pop_back();
+		}
+		if (left < depth)
+		{
+			cur.push_back('(');
+			backtrack(ans, cur, left + 1, right, depth);
+			cur.pop_back();
+		
+		}
+
+		
+	}
+public:
+	vector<string>ge(int n)
+	{
+		vector<string> ans;
+		string cur;
+		backtrack(ans, cur, 0,0,n);
+		return ans;
+	}
+};
+
