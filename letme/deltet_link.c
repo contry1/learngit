7baae90da9fdd0804bba1b70cffe78c4e76238af
contry1 <3178488406@qq.com>
2020-12-01 20:58:30 +0800

    Update deltet_link.c
    add kuohao

--- letme/deltet_link.c
+++ letme/deltet_link.c
@@ -31,3 +31,41 @@ public:
 
     }
 };
+
+
+
+class Solution {
+public:
+    vector<string> ans;
+    string path;
+   void dfs(int left,int right,int n )
+    {
+       if(left == n &&right ==n)
+       {
+           ans.push_back(path);
+           return;
+       }
+       if(right>left)
+            return;
+        for(int i = left;i<n;++i)
+        {
+            if(i>left &&path[i] == path[i-1])
+            continue;
+            path.push_back('(');
+            dfs(left+1,right,n);
+            path.pop_back();
+        }
+          for(int i = right;i<n;++i)
+        {
+             if(i>right &&path[i] == path[i-1])
+            continue;
+            path.push_back(')');
+            dfs(left,right+1,n);
+            path.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        dfs(0,0,n);
+        return ans;
+    }
+};
