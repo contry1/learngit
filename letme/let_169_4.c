0d64612c914c84d2c5b58f0ae592775a8e88550c
contry1 <3178488406@qq.com>
2020-12-05 14:39:46 +0800

    Create let_169_4.c
    let 1694

---
+++ letme/let_169_4.c
@@ -0,0 +1,51 @@
+class solution_169 {
+public:
+
+	int get_many(vector<int> nums)
+	{
+		int len = nums.size();
+		/*	unordered_map<int, int>mp;
+			for (int &ch : nums)
+			{
+				mp[ch]++;
+				if (mp[ch] > len >> 1)
+					return ch;
+			}*/
+
+			/*	sort(nums.begin(), nums.end());
+				return nums[len >> 1];*/
+		int index = 0;
+		
+		while (1)
+		{
+			int many = nums[index];
+			int cnt = 0;
+			for (int i = 0; i < len; ++i)
+			{
+				if (nums[i] == many)
+				{
+					cnt++;
+				}
+				else
+				{
+					cnt--;
+				}
+
+
+			}
+
+			if (cnt > 0)
+			{
+				return many;
+			}
+
+			else
+			{
+				index++;
+				
+			}
+		}
+		
+		return 0;
+	}
+};
