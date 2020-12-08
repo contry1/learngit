72dd08e158efb6c2e7d0ffcd6b67ea6cecf3b180
contry1 <3178488406@qq.com>
2020-12-08 10:32:44 +0800

    Create let_40_3.c
    40demo

---
+++ letme/let_40_3.c
@@ -0,0 +1,37 @@
+class solution_40 {
+public:
+	vector<int> path;
+	vector<vector<int>> ans;
+	void dfs(vector<int> &nums, int start, int size, int target)
+	{
+		if (target == 0)
+		{
+			ans.push_back(path);
+			return;
+		}
+		for (int i = start; i < size; ++i)
+		{
+			if (i > start && nums[i] == nums[i - 1])
+			{
+				cout << "=========:" << nums[i] << endl;
+				continue;
+			}
+		
+			if (target < nums[i])
+			{
+				continue;
+			}
+			path.push_back(nums[i]);
+			//start = i;
+			dfs(nums, i + 1, size, target - nums[i]);
+			path.pop_back();
+		}
+	}
+	vector<vector<int>> get_left_index(vector<int> &nums, int target)
+	{
+		int len = nums.size();
+		sort(nums.begin(), nums.end()); //quchong
+		dfs(nums, 0, len, target);
+		return ans;
+	}
+};
