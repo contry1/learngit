bc9f3e3b9e8721422e8db99f3d438e096a52c3f7
contry1 <3178488406@qq.com>
2020-12-10 21:05:58 +0800

    Update letme_1_0_6.c
    update

--- letme/letme_1_0_6.c
+++ letme/letme_1_0_6.c
@@ -1,41 +1,50 @@
-class solution_106 {
+class Solution {
 public:
-	string is_str(string str)
-	{
-		if (str.length() == 0)
-			return str;
-	
-		int cnt = 0;
-		char c = str[0];
-		string ans;
-		for (int i = 0; i < str.length(); ++i)
-		{
-			if (str[i] == c)
-			{
+    string compressString(string S) {
+        string str = S;
+if (str.length() == 0)
+            return str;
 
-				cnt++;
-			}
-			else
-			{
-				ans.push_back(c);
-				ans.push_back(cnt + 48);
-				c = str[i];
-				cnt = 1;
-		
-			}
+        int cnt = 0;
+        char c = str[0];
+        string ans;
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (str[i] == c)
+            {
 
-		}
+                cnt++;
+            }
+            else
+            {
+                ans.push_back(c);
 
-		//mp.emplace_hint(mp.begin(),c+1, cnt);
-		//key is same ,so used mutilmap,but mutilmap insert and  emplace_hint is seriers,so it can't be used!
+                string num = to_string(cnt);
+                for(char &ch:num)
+                    ans.push_back(ch);
+                    //ans.push_back(cnt + 48);
+                c = str[i];
+                cnt = 1;
+
+            }
+
+        }
+
+        //mp.emplace_hint(mp.begin(),c+1, cnt);
+        //key is same ,so used mutilmap,but mutilmap insert and  emplace_hint is seriers,so it can't be used!
 
 
-		ans.push_back(c);
-		ans.push_back(cnt + 48);
-		cout << str.length() << ans.size() << endl;
-		if (str.length() <= ans.size())
-			return str;
-		else
-			return ans;
-	}
+        ans.push_back(c);
+    //  ans.push_back(cnt + 48); 
+    //line 38这里用line39-line41 代替
+        string num2 = to_string(cnt);
+                for(char &ch:num2)
+                    ans.push_back(ch);
+
+        cout << str.length() << ans.size() << endl;
+        if (str.length() <= ans.size())
+            return str;
+        else
+            return ans;
+    }
 };
